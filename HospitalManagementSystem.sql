dca5d767241b94dc436edb3460d3add95b1018a4
Amani Khalid <amanikhalidd@gmail.com>
2025-06-25 09:12:33 +0400

    Created Patients Table

--- HospitalManagementSystem.sql
+++ HospitalManagementSystem.sql
@@ -9,3 +9,14 @@ CREATE TABLE Departments (
 Department_ID INT PRIMARY KEY IDENTITY(1,1),
 Department_Name VARCHAR(100) NOT NULL
 );
+
+-- Patients Table
+CREATE TABLE Patients (
+P_ID INT PRIMARY KEY IDENTITY (1,1),
+F_Name VARCHAR(50) NOT NULL,
+M_Name VARCHAR(50) NOT NULL,
+L_Name VARCHAR(50) NOT NULL,
+DOB DATE NOT NULL,
+Gender VARCHAR(10) CHECK (Gender IN ('Male', 'Female')),
+ContactNumber VARCHAR(20) UNIQUE
+);

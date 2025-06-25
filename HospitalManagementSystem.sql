bed859618071935622bc95a477e8168a9667edb3
Amani Khalid <amanikhalidd@gmail.com>
2025-06-25 11:17:21 +0400

    Security/ GRANT INSERT, UPDATE for AdminUser on all tables

--- HospitalManagementSystem.sql
+++ HospitalManagementSystem.sql
@@ -632,3 +632,16 @@ CREATE ROLE AdminUser;
 GRANT SELECT ON Patients TO DoctorUser;
 GRANT SELECT ON Appointments TO DoctorUser;
 
+--GRANT INSERT, UPDATE for AdminUser on all tables
+GRANT INSERT, UPDATE ON Patients TO AdminUser;
+GRANT INSERT, UPDATE ON Doctors TO AdminUser;
+GRANT INSERT, UPDATE ON Departments TO AdminUser;
+GRANT INSERT, UPDATE ON Appointments TO AdminUser;
+GRANT INSERT, UPDATE ON MedicalRecords TO AdminUser;
+GRANT INSERT, UPDATE ON Billing TO AdminUser;
+GRANT INSERT, UPDATE ON Rooms TO AdminUser;
+GRANT INSERT, UPDATE ON Staff TO AdminUser;
+GRANT INSERT, UPDATE ON Admissions TO AdminUser;
+GRANT INSERT, UPDATE ON Users TO AdminUser;
+
+

7ce35572ea62c7eaf423b9950795da87a8797bf6
Amani Khalid <amanikhalidd@gmail.com>
2025-06-25 09:15:38 +0400

    Created Billing Table

--- HospitalManagementSystem.sql
+++ HospitalManagementSystem.sql
@@ -1,16 +1,16 @@
--- Create database
+--Create database
 CREATE DATABASE HospitalMS;
 
--- Use the database
+--Use the database
 USE HospitalMS;
 
--- Departments Table
+--Departments Table
 CREATE TABLE Departments (
 Department_ID INT PRIMARY KEY IDENTITY(1,1),
 Department_Name VARCHAR(100) NOT NULL
 );
 
--- Patients Table
+--Patients Table
 CREATE TABLE Patients (
 P_ID INT PRIMARY KEY IDENTITY (1,1),
 F_Name VARCHAR(50) NOT NULL,
@@ -21,7 +21,7 @@ Gender VARCHAR(10) CHECK (Gender IN ('Male', 'Female')),
 ContactNumber VARCHAR(20) UNIQUE
 );
 
--- Doctors Table
+--Doctors Table
 CREATE TABLE Doctors (
 D_ID INT PRIMARY KEY IDENTITY(1,1),
 F_Name VARCHAR(50) NOT NULL,
@@ -32,7 +32,7 @@ ContactNumber VARCHAR(20),
 Department_ID INT NOT NULL FOREIGN KEY REFERENCES Departments(Department_ID)
 );
 
--- Appointments Table
+--Appointments Table
 CREATE TABLE Appointments (
 Appointment_ID INT PRIMARY KEY IDENTITY(1,1),
 P_ID INT NOT NULL FOREIGN KEY REFERENCES Patients(P_ID),
@@ -41,7 +41,7 @@ Appointment_Date DATETIME NOT NULL,
 Appointment_Time DATETIME NOT NULL
 );
 
--- MedicalRecords Table
+--MedicalRecords Table
 CREATE TABLE MedicalRecords (
 M_ID INT PRIMARY KEY IDENTITY(1,1),
 P_ID INT NOT NULL FOREIGN KEY REFERENCES Patients(P_ID),
@@ -50,4 +50,14 @@ Diagnosis VARCHAR(200) NOT NULL,
 Treatment_Plans VARCHAR(600) NOT NULL,
 VisitDate DATE NOT NULL,
 Notes VARCHAR(1000)
+);
+
+
+--Billing Table
+CREATE TABLE Billing (
+B_ID INT PRIMARY KEY IDENTITY(1,1),
+P_ID INT NOT NULL FOREIGN KEY REFERENCES Patients(P_ID),
+Total_Cost DECIMAL(10,2) NOT NULL,
+BServices VARCHAR(500),
+BillDate DATE NOT NULL
 );

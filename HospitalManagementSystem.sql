2b9e8189a57e456b1c9633e0817431c7d2aec210
Amani Khalid <amanikhalidd@gmail.com>
2025-06-25 09:11:26 +0400

    Created Departments Table

--- HospitalManagementSystem.sql
+++ HospitalManagementSystem.sql
@@ -1,2 +1,11 @@
 -- Create database
 CREATE DATABASE HospitalMS;
+
+-- Use the database
+USE HospitalMS;
+
+-- Departments Table
+CREATE TABLE Departments (
+Department_ID INT PRIMARY KEY IDENTITY(1,1),
+Department_Name VARCHAR(100) NOT NULL
+);
